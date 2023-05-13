6f53564b32b45b0550e72d796491291fab2224da
Emil Rekola <emil.rekola@hotmail.com>
2023-05-14 01:35:44 +0300

    Created empty base project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="p-6 max-w-sm mx-auto bg-white dark:bg-black rounded-xl shadow-lg flex items-center space-x-4">
+        <p className="text-slate-500 dark:text-slate-400">
+          The most awesomest wedding website!
+        </p>
+      </div>
+    </main>
+  );
+}
