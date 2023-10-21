40bdbde78c28a4b155b1e2f51b53ae57acc4ac9a
Emil Rekola <emil.rekola@hotmail.com>
2023-10-21 09:47:23 +0300

    Added thankyou page and updated .gitignore

---
+++ app/kiitos/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function ThankYou() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => router.push("/"), 5000);
+  }, []);
+
+  return (
+    <section id="thanks-section" className="section-primary">
+      <h1 className="h1-header">Kiitos vierailusta!</h1>
+      <h2 className="mb-6 text-center text-2xl tracking-tighter">
+        Toivottavasti nähdään häissä!
+      </h2>
+    </section>
+  );
+}
